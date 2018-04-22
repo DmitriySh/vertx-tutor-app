90fe149a9eca6947c7fc991fbfd82431dddcc9bd
DmitriySh <dashishmakov@gmail.com>
2018-04-22 17:16:53 +0300

    Add first verticle of vert.x framework

---
+++ src/main/java/ru/shishmakov/FirstVerticle.java
@@ -0,0 +1,17 @@
+package ru.shishmakov;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class FirstVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) {
+        vertx.createHttpServer()
+                .requestHandler(r -> r.response().end("<h1>Hello from my first Vert.x 3 application</h1>"))
+                .listen(8080, result -> {
+                    if (result.succeeded()) startFuture.complete();
+                    else startFuture.fail(result.cause());
+                });
+    }
+}
