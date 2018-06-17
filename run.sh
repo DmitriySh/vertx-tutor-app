3c1e04341c2042334683f342f5e4e5795f052127
DmitriySh <dashishmakov@gmail.com>
2018-06-18 01:17:08 +0300

    change run command

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ export main_verticle="ru.shishmakov.WebMongoVerticle"
 #export main_verticle="ru.shishmakov.WebSqlVerticle"
 #export main_verticle="ru.shishmakov.SimpleVerticle"
 
-export compile_verify="compile verify"
-export compile_package="compile package"
 
-./mvnw $compile_verify
+if [[ -n "$1" && "$1" = "build" ]]; then
+	./mvnw clean verify
+fi
 
 java -jar ./target/vertx-tutor-app-1.0-SNAPSHOT-fat.jar
+
