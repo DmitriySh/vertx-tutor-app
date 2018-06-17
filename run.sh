4f154d3583a4cc49cce03086286442b01687bba8
DmitriySh <dashishmakov@gmail.com>
2018-06-18 02:09:04 +0300

    Use a config file on time script execution

--- run.sh
+++ run.sh
@@ -5,10 +5,23 @@ export main_verticle="ru.shishmakov.WebMongoVerticle"
 #export main_verticle="ru.shishmakov.WebSqlVerticle"
 #export main_verticle="ru.shishmakov.SimpleVerticle"
 
+conf=""
+for key in "$@"
+do
+	echo $key
+	if [[ -n "$key" && "$key" = "build" ]]; then
+		./mvnw clean verify
+	fi
+	if [[ -n "$key" && -f "$key" ]]; then
+		conf=$key
+	fi
+done
 
-if [[ -n "$1" && "$1" = "build" ]]; then
-	./mvnw clean verify
+
+if [[ -n "$conf" ]]
+then
+	java -jar ./target/vertx-tutor-app-1.0-SNAPSHOT-fat.jar -conf $conf
+else
+	java -jar ./target/vertx-tutor-app-1.0-SNAPSHOT-fat.jar
 fi
-
-java -jar ./target/vertx-tutor-app-1.0-SNAPSHOT-fat.jar
 
